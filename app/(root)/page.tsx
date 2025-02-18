2bde17f63fa349b0d8ae343ab0d070e2cb7fd180
Khoa Phan <tkcuakhoachamchi@gmail.com>
2025-02-19 01:02:27 +0700

    basic layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
-
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const Homepage = async () => {
   return (
-    <>Pro</>
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </>
   );
-}
+};
 export default Homepage;
