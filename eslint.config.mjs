5dbd3e34e74ce72743bbcd14854b023976336590
Khoa Phan <tkcuakhoachamchi@gmail.com>
2025-02-26 23:33:02 +0700

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "@typescript-eslint/no-empty-interface": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
